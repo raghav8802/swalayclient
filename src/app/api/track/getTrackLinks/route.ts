e2a66c128851d9f53e85f8d9ed5c26e5f77e032e
SayanSutradhar1 <sayan321x@gmail.com>
2025-07-16 17:55:46 +0530

    minor

--- src/app/api/track/getTrackLinks/route.ts
+++ src/app/api/track/getTrackLinks/route.ts
@@ -61,7 +61,7 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({
       success: false,
       message: "Failed to fetch track links",
-      status: 500,
+      status: 404,
     },{
       status : 404
     });
